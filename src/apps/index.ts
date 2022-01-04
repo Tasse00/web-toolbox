e7d0f80af0152dfd5dacfe226e5b47a8ad91438c
zhangzheng <zhangzheng@aengine.com.cn>
2022-01-04 10:37:00 +0800

    feat(app): JsonViewer

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -1,12 +1,18 @@
 import { AppConfig } from "../framework/Framework";
+import { JsonViewApp } from "./JsonView";
 import { Web3UtilsApp } from "./Web3Utils";
 
 const configs: AppConfig[] = [
   {
-    appId: 'web3-utils',
+    appId: "web3-utils",
     title: "Web3Utils",
     component: Web3UtilsApp,
-  }
-]
+  },
+  {
+    appId: "json-viewer",
+    title: "JsonViewer",
+    component: JsonViewApp,
+  },
+];
 
-export default configs
+export default configs;
